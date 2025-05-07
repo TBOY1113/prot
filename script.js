28e41f53383272fbc527d89cb337735254f819a7
TBOY <tharulbandara123@gmail.com>
2025-05-07 20:03:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+//Work selector and work changer according to the selected category
+
+const workNames = document.querySelectorAll('.pg3 .wrapperPageThree .topseg .works  li');
+const progsInterfaceGraphic = document.getElementById('progsInterfaceGraphic');
+const progsInterfacePhotography = document.getElementById('progsInterfacePhotography');
+const progsInterfaceVideography = document.getElementById('progsInterfaceVideography');
+
+
+workNames.forEach((workName)=> {
+        workName.addEventListener('click', ()=>{
+            if (workName.id === 'graphicsWork'){
+                progsInterfaceGraphic.style.display = 'flex';
+                progsInterfacePhotography.style.display = 'none';
+                progsInterfaceVideography.style.display = 'none';
+                
+            }
+            else if (workName.id === 'photographyWork') {
+                progsInterfacePhotography.style.display = 'flex';
+                progsInterfaceVideography.style.display = 'none';
+                progsInterfaceGraphic.style.display = 'none';
+            }
+            else if (workName.id == 'videditWork'){
+                progsInterfaceVideography.style.display = 'flex';
+                progsInterfacePhotography.style.display = 'none';
+                progsInterfaceGraphic.style.display = 'none';
+            }
+        });
+});
